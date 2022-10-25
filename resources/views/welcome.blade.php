35de67cae554b91d64f3f3cafcd1230625702e96
userlg <guipeluis90@gmail.com>
2022-10-25 13:51:19 -0400

    Livewire added correctly

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SGMP</title>
     @vite(['resources/js/app.js'])
+    <livewire:styles />
 </head>
+
 <body>
 
     <h2 class="font-bold text-center mb-10 text-pink-500">Testing tailwind Css</h2>
-    
+
+
+    <livewire:scripts />
 </body>
+
 </html>
