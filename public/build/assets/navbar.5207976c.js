0d084996800542bedd40e0c6128992954ee24e51
userlg <guipeluis90@gmail.com>
2022-11-12 07:12:44 -0400

    Improved on the gui

---
+++ public/build/assets/navbar.5207976c.js
@@ -0,0 +1,1 @@
+const t=document.getElementById("navbarButton"),e=document.getElementById("navbarSupportedContent");var s=!1;t.addEventListener("click",()=>{s?(s=!1,e.classList.remove("visible"),e.classList.add("collapse")):(s=!0,e.classList.remove("collapse"),e.classList.add("visible"))});
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,15 @@
 <x-layouts.base title="Acerca">
-    <p class="text-green-400 font-semibold text-center">About component</p>
+
+    <div
+        class="flex flex-col mx-auto p-4 justify-center items-center gap-2 bg-gray-100 shadow-sm shadow-gray-400 dark:shadow-none dark:bg-night dark:ring-1 dark:ring-sred w-3/4 md:w-1/2 rounded-sm dark:border-none border border-black">
+        <h2 class="text-sm md:text-lg capitalize text-sred font-bold text-center">Sistema Generico de Administracion de
+            Proyectos</h2>
+        <p class="dark:text-white text-justify">Sistema que permite la administracion de Proyectos mediante creacion de
+            tareas
+            y cambio de estados de las tareas.
+            Haciendo uso de este sistema usted podra simplificar el trabajo y la asignacion de responsabilidades para
+            cualquier proyecto.
+        </p>
+    </div>
+
 </x-layouts.base>
