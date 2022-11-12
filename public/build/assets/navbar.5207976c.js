f3f14c2b07b301fd7810049d1941df78889b25ef
userlg <guipeluis90@gmail.com>
2022-11-12 08:14:18 -0400

    New buikd generated

--- public/build/assets/navbar.5207976c.js
+++
@@ -1,1 +0,0 @@
-const t=document.getElementById("navbarButton"),e=document.getElementById("navbarSupportedContent");var s=!1;t.addEventListener("click",()=>{s?(s=!1,e.classList.remove("visible"),e.classList.add("collapse")):(s=!0,e.classList.remove("collapse"),e.classList.add("visible"))});
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js', 'resources/js/dark.js', 'resources/js/navbar.js'],
+            input: ['resources/scss/app.scss', 'resources/js/app.js', 'resources/js/dark.js'],
             refresh: true,
         }),
     ],
