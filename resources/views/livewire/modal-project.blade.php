d332a85350355f769e18e2c75b9830b9497ee2ed
userlg <guipeluis90@gmail.com>
2022-12-01 11:39:51 -0400

    New update to the project

--- resources/views/livewire/modal-project.blade.php
+++ resources/views/livewire/modal-project.blade.php
@@ -3,10 +3,12 @@
     <div @if ($open == true) class="visible"
           @else
     class="hidden" @endif>
-        <div class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
-            <div class="bg-white dark:bg-night px-16 dark:shadow-md dark:shadow-sred py-14 rounded-md text-center">
-                <h1 class="text-xl mb-4 font-semibold font-pacifico text-slate-500 dark:text-sred">Crear Proyecto</h1>
-                <button class="bg-sred px-7 py-2 ml-2 rounded-md text-md text-white font-semibold"
+        <div
+            class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0 animate-fade-in-down">
+            <div
+                class="bg-white dark:bg-night px-16 dark:shadow-md dark:shadow-sred py-14 rounded-md text-center w-11/12 md:w-1/3 max-w-lg">
+                <h1 class="text-xl mb-4 font-semibold font-pacifico text-black dark:text-sred">Crear Proyecto</h1>
+                <button class="bg-sred px-7 py-2 ml-2 rounded-md text-md text-white text-sm md:text-base font-semibold"
                     wire:click="close">Cerrar</button>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,30 @@ module.exports = {
       'pacifico': ['Pacifico', 'cursive'],
     },
     extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': {
+            transform: 'rotate(-3deg)'
+          },
+          '50%': {
+            transform: 'rotate(3deg)'
+          },
+        },
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        'fade-in-down': 'fade-in-down 0.5s ease-out'
+      },
       colors: {
         night: '#0A0E22',
         sred: '#FF2D20',
