094554b30cb751e7fb2531ac8fcca14b5dec783b
userlg <guipeluis90@gmail.com>
2022-12-16 14:27:18 -0400

    Improved done on the template login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,24 +2,24 @@
     {{-- Login FORM --}}
     <div class="mt-40">
         <form action="{{ route('login') }}" method="POST"
-            class="border border-black dark:border-sred rounded-md  dark:shadow-none bg-white shadow-md shadow-gray-400 dark:bg-night w-11/12 md:w-1/5 mx-auto flex flex-col p-3 justify-center items-center gap-4 ">
-            <h2 class="text-sred font-bold text-center text-md md:text-2xl">Login</h2>
+            class="border border-black dark:border-sred rounded-md  dark:shadow-none  bg-white shadow-md shadow-gray-400 dark:bg-night w-3/4 sm:w-1/2 md:w-[300px] mx-auto flex flex-col p-8 justify-center items-center gap-4 ">
+            <h2 class="text-sred font-bold text-center text-xl md:text-2xl font-pacifico">Login</h2>
             @csrf
-            <label for="email" class="flex flex-col justify-center items-center">
+            <label for="email" class="flex flex-col justify-center items-center w-3/4 md:w-11/12">
                 <input type="email" placeholder="email" name="email"
-                    class="border border-black rounded dark:text-black w-full" value="{{ old('email') }}">
+                    class="border border-black rounded dark:text-black w-full text-center" value="{{ old('email') }}">
                 @error('email')
                     <p class="dark:text-sred">{{ $message }}</p>
                 @enderror
             </label>
-            <label for="password" class="flex flex-col justify-center items-center">
+            <label for="password" class="flex flex-col justify-center items-center w-3/4 md:w-11/12">
                 <input type="password" placeholder="password" name="password"
-                    class="border border-black rounded w-full text-black">
+                    class="border border-black rounded w-full text-black text-center">
                 @error('password')
                     <p class="dark:text-sred">{{ $message }}</p>
                 @enderror
             </label>
-            <button class="text-white text-sm sm:text-base bg-sred p-2 rounded  w-1/2 sm:w-1/4">Entrar</button>
+            <button class="text-white text-sm sm:text-base bg-sred p-2 rounded  w-28">Entrar</button>
         </form>
     </div>
     {{-- ************ --}}
